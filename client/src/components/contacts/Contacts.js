7530f45f2de853c36826ee8736601ca39a6e2f8f
gregggarrison <gregg.garrison@rocketmail.com>
2021-05-09 12:18:08 -0600

    added auth context

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
 
@@ -14,10 +14,43 @@ const Contacts = () => {
 
     return (
         <>
-            {filtered !== null
-                ? filtered.map(contact => (<ContactItem key={contact.id} contact={contact} />))
-                : contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)
-            }
+            {/* <TransitionGroup>
+
+                {filtered !== null
+                    ? filtered.map(contact => (
+                        <CSSTransition key={contact.id} timeout={500} classNames="item">
+                            <ContactItem contact={contact} />))
+                        </CSSTransition>
+                    ))
+                    : contacts.map(contact => (
+                        <CSSTransition key={contact.id} timeout={500} classNames="item">
+                            <ContactItem contact={contact} />)
+                        </CSSTransition>
+                    ))
+                }
+            </TransitionGroup> */}
+            <TransitionGroup>
+                {filtered !== null
+                    ? filtered.map(contact => (
+                        <CSSTransition
+                            key={contact.id}
+                            timeout={500}
+                            classNames='item'
+                        >
+                            <ContactItem contact={contact} />
+                        </CSSTransition>
+                    ))
+                    : contacts.map(contact => (
+                        <CSSTransition
+                            key={contact.id}
+                            timeout={500}
+                            classNames='item'
+                        >
+                            <ContactItem contact={contact} />
+                        </CSSTransition>
+                    ))}
+            </TransitionGroup>
+
         </>
     );
 };
