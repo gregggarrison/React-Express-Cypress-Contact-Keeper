b9fb10ef92394dcbf4ea261fd1a0a3ae8b82a999
gregggarrison <gregg.garrison@rocketmail.com>
2021-05-14 13:41:03 -0600

    tidy

--- cypress/support/pageObjects/loginPage.js
+++ cypress/support/pageObjects/loginPage.js
@@ -1,7 +1,5 @@
 
-
 export class LoginPage {
-
     submitLoginFormValidCredentials(email, password) {
         cy.get('.form-group').find('[name="email"]').type(email);
         cy.get('.form-group').find('[type="password"]').type(password);
@@ -9,16 +7,10 @@ export class LoginPage {
         cy.get('[id="cy-logout"]').then(logout => {
             expect(logout.text()).to.equal('Logout')
         })
-
- 
     }
-
     logout(){
         cy.get('[id="cy-logout"]').click()
     }
-    
-    
-
 }
 
 export const onLoginPage = new LoginPage();
